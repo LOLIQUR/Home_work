9e960600b62a29702628e39518c255a4d0755a6e
Danil <mrjunkboy@vk.com>
2025-05-12 02:34:50 +0300

    Создана функция sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -3,3 +3,10 @@ def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[d
     Фильтрует список словарей по значению ключа 'state'
     """
     return [t for t in transactions if t.get('state') == state]
+
+
+def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Сортирует список словарей по дате.
+    """
+    return sorted(transactions, key=lambda x: x['date'], reverse=reverse)
