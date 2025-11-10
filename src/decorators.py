defc9f494e0dc2047191045810f89bd871296fa1
Danil <mrjunkboy@vk.com>
2025-11-10 16:16:24 +0300

    Реализовал декоратор log с логированием в файл и консоль

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,8 @@
 """
 Модуль для работы с декораторами.
 """
+import functools
+from datetime import datetime
 
 
 def log(filename=None):
@@ -13,4 +15,41 @@ def log(filename=None):
     Returns:
         Декорированную функцию
     """
-    pass
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # Формируем сообщение о вызове функции
+            func_name = func.__name__
+
+            try:
+                # Выполняем функцию
+                result = func(*args, **kwargs)
+
+                # Формируем сообщение об успехе
+                log_message = f"{func_name} ok\n"
+
+                # Записываем в файл или выводим в консоль
+                if filename:
+                    with open(filename, 'a', encoding='utf-8') as f:
+                        f.write(log_message)
+                else:
+                    print(log_message, end='')
+
+                return result
+
+            except Exception as e:
+                # Формируем сообщение об ошибке
+                error_message = f"{func_name} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n"
+
+                # Записываем в файл или выводим в консоль
+                if filename:
+                    with open(filename, 'a', encoding='utf-8') as f:
+                        f.write(error_message)
+                else:
+                    print(error_message, end='')
+
+                # Пробрасываем исключение дальше
+                raise
+
+        return wrapper
+    return decorator
