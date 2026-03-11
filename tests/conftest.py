c34d5a8cf7a363c98dc7b4d9a8053f9524bb4222
Danil <mrjunkboy@vk.com>
2026-03-11 22:40:00 +0300

    Добавил main.py и обновил фикстуру в conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,10 @@ import pytest
 
 @pytest.fixture
 def sample_transactions():
+    """Фикстура с транзакциями, содержащими description."""
     return [
-        {"id": 1, "state": "EXECUTED", "date": "2023-01-01"},
-        {"id": 2, "state": "CANCELED", "date": "2023-01-02"},
-        {"id": 3, "state": "EXECUTED", "date": "2023-01-03"},
+        {"id": 1, "state": "EXECUTED", "date": "2023-01-01", "description": "Перевод организации"},
+        {"id": 2, "state": "CANCELED", "date": "2023-01-02", "description": "Перевод с карты на карту"},
+        {"id": 3, "state": "EXECUTED", "date": "2023-01-03", "description": "Открытие вклада"},
+        {"id": 4, "state": "EXECUTED", "date": "2023-01-04", "description": "Перевод со счета на счет"},
     ]
